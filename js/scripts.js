c00abbaae0f36b37956f1b0d1071550e102c541c
Viktor Ermolov <viktor@ermolov.dev>
2021-08-18 11:57:48 +0000

    format js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 // Set the date we're counting down to
 var countDownDate = new Date("Sep 10, 2021 12:00:00").getTime();
 
-        // Update the count down every 1 second
-var x = setInterval(function() {
+// Update the count down every 1 second
+var x = setInterval(function () {
 
   // Get today's date and time
   var now = new Date().getTime();
@@ -17,12 +17,12 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("counter").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("counter").innerHTML = days + "d " + hours + "h " +
+    minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
-            clearInterval(x);
+    clearInterval(x);
     document.getElementById("counter").innerHTML = "EXPIRED";
   }
 }, 1000);
